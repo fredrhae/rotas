f9dd55b55a9be2329975d03b79683af2ae680efa
Marivaldo Júnior <marivaldoeng@gmail.com>
2016-05-25 14:41:46 -0300

    estrutura inicial do algoritimo Teitz & Bart.

--- Rotas/Rotas/teitz_bart.cpp
+++ Rotas/Rotas/teitz_bart.cpp
@@ -4,13 +4,63 @@ namespace rotas
 {
 	namespace algoritmos
 	{
-		TeitzBart::TeitzBart()
+		std::vector<Cidade> TeitzBart::localiza_medianas(const std::vector<Cidade>& cidades)
 		{
+			lista_medianas_t medianas = seleciona_medianas_aleatoriamente(cidades); // conjunto 'S'
+			lista_vertices_t para_analisar = vertices_para_analisar(medianas); // { V - S }
+
+			while (todos_foram_analisados(para_analisar) == false)
+			{
+				for (unsigned int i = 0; i < para_analisar.size(); i++)
+				{
+					vertice_t vertice = para_analisar.at(i);
+
+					bool modificou = analisa_vertice(vertice, medianas);
+
+					if (modificou == true)
+					{
+						para_analisar = vertices_para_analisar(medianas);
+					}
+				}
+			}
+
+			return medianas;
 		}
 
-
-		TeitzBart::~TeitzBart()
+		lista_medianas_t TeitzBart::seleciona_medianas_aleatoriamente(const std::vector<Cidade>& cidades)
 		{
+			// TODO
+
+			return lista_medianas_t();
+		}
+
+		lista_vertices_t TeitzBart::vertices_para_analisar(const lista_medianas_t& medianas)
+		{
+			// TODO
+
+			return lista_vertices_t();
+		}
+
+		bool TeitzBart::todos_foram_analisados(const lista_vertices_t& vertices)
+		{
+			for (unsigned int i = 0; i < vertices.size(); i++)
+			{
+				vertice_t v = vertices.at(i);
+
+				if (v.analisado == false)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		bool TeitzBart::analisa_vertice(vertice_t& vertice, lista_medianas_t& medianas)
+		{
+			// TODO
+
+			return false;
 		}
 	} // algoritmos
 } // rotas
--- Rotas/Rotas/teitz_bart.h
+++ Rotas/Rotas/teitz_bart.h
@@ -3,18 +3,63 @@
 
 #include <vector>
 #include "domain/caminho.h"
+#include "domain/cidade.h"
 
 namespace rotas
 {
 	namespace algoritmos
 	{
+		namespace teitz_bart
+		{
+			typedef struct vertice_t
+			{
+				domain::Cidade cidade;
+				bool analisado;
+
+				vertice_t() : analisado(false) {}
+				vertice_t(const domain::Cidade& c, bool a = false) : cidade(c), analisado(a) {}
+			} vertice_t;
+
+			typedef std::vector<vertice_t> lista_vertices_t;
+
+			typedef domain::Cidade mediana_t;
+
+			typedef std::vector<mediana_t> lista_medianas_t;
+		}
+
 		using namespace domain;
+		using namespace teitz_bart;
 
 		class TeitzBart
 		{
 		public:
-			TeitzBart();
-			~TeitzBart();
+			inline TeitzBart() { }
+			inline ~TeitzBart() { }
+
+			std::vector<Cidade> localiza_medianas(const std::vector<Cidade>& cidades);
+
+		private:
+
+			/**
+			 * Seleciona aleatoriamente um conjunto 'S', com tamanho 'p' (número de cidades utilizadas como medianas)
+			 * para formar uma aproximação inicial para as p-medianas.
+			 */
+			lista_medianas_t seleciona_medianas_aleatoriamente(const std::vector<Cidade>& cidades);
+
+			/**
+			 * Recupera a lista de vértices para serem analisadas pelo algoritmo.
+			 */
+			lista_vertices_t vertices_para_analisar(const lista_medianas_t& medianas);
+
+			/**
+			 * Informa se todos os vértices foram analisados.
+			 */
+			bool todos_foram_analisados(const lista_vertices_t& vertices);
+
+			/**
+			 * Analisa se é uma mediana melhor. Retorna true se houve modificações no conjunto 'S'.
+			 */
+			bool analisa_vertice(vertice_t& vertice, lista_medianas_t& medianas);
 		};
 	} // algoritmos
 } // rotas
